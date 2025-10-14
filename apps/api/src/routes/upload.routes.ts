b5b734493d7896aa1169a062d126f18ca2342284
francescastewart <francesca.stewart@unb.ca>
2025-10-14 15:31:26 -0300

    14 - Update files for docker

--- apps/api/src/routes/upload.routes.ts
+++ apps/api/src/routes/upload.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import multer from "multer";
+//import multer from "multer";
 
 const router = Router();
-const upload = multer({ dest: "uploads/" }); // stores files locally
+//const upload = multer({ dest: "uploads/" }); // stores files locally
 
-router.post("/", upload.array("files"), (req, res) => {
+/*router.post("/", upload.array("files"), (req, res) => {
   const files = req.files as Express.Multer.File[];
   if (!files || files.length === 0) {
     return res.status(400).json({ message: "No files uploaded" });
@@ -15,7 +15,7 @@ router.post("/", upload.array("files"), (req, res) => {
   console.log("Files uploaded:", req.files);
 
   res.json({ message: "Files uploaded successfully!", files: req.files });
-});
+});*/
 
 router.get("/", (_req, res) => {
   res.json({ message: "Upload route is running!" });
