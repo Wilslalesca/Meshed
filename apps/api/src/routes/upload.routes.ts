d1c50321f3ce3efb78be0822036f0445622080e6
francescastewart <francesca.stewart@unb.ca>
2025-10-16 21:11:50 -0300

    14 - Update upload.route to not store files locally

--- apps/api/src/routes/upload.routes.ts
+++ apps/api/src/routes/upload.routes.ts
@@ -5,7 +5,8 @@ import { format } from "date-fns";
 import { toZonedTime } from "date-fns-tz"; //Because the dates are formated in UTC it gets wonky during daylight savings so we have to convert
 
 const router = Router();
-const upload = multer({ dest: "uploads/" }); // stores files locally
+//const upload = multer({ dest: "uploads/" }); // stores files locally
+const upload = multer({ storage: multer.memoryStorage() });
 const timeZone = "America/Halifax"; // Atlantic Standard Time 
 
 router.post("/", upload.array("files"), (req, res) => {
@@ -26,7 +27,8 @@ router.post("/", upload.array("files"), (req, res) => {
   if(schedule.originalname.endsWith(".ics")){
     try{
       var count = 0;
-      const parsedFile= ical.sync.parseFile(schedule.path);
+      const icsData = schedule.buffer.toString("utf-8");
+      const parsedFile = ical.parseICS(icsData);
       for (const key in parsedFile){
         const course = parsedFile[key];
         if(course.type == "VEVENT"){
