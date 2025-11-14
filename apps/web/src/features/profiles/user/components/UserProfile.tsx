95ffa477352f4994d657bdb6889c9b8be06c8f93
francescastewart <francesca.stewart@unb.ca>
2025-11-14 14:31:00 -0400

    Update UserProfile.tsx

--- apps/web/src/features/profiles/user/components/UserProfile.tsx
+++ apps/web/src/features/profiles/user/components/UserProfile.tsx
@@ -3,7 +3,6 @@ import {useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
-import { useAuth } from '@/hooks/useAuth';
 import { Button } from "@/components/ui/button"
 import { Pencil, Check } from "lucide-react";
 import {apiEditUser} from "../apis/editprofile"
