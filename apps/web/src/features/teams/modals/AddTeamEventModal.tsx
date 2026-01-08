1b4fb0debaed2bfd5c29bfcafe2e2e719ae13235
francescastewart <francesca.stewart@unb.ca>
2026-01-08 09:28:33 -0400

    Update form inputs on add team event modal

--- apps/web/src/features/teams/modals/AddTeamEventModal.tsx
+++ apps/web/src/features/teams/modals/AddTeamEventModal.tsx
@@ -10,6 +10,7 @@ import { toast } from "sonner"
 import type { EventItem } from "@/features/dashboard/types/eventItem"
 import { Label } from "@/shared/components//ui/label";
 import { EventWidget } from "@/features/dashboard/components/EventWidget";
+import { Input } from "@/shared/components//ui/input";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogTrigger } from '@/shared/components/ui/dialog';
 import {
     Select,
@@ -46,6 +47,10 @@ export const AddTeamEventModal = ({ open, onOpenChange, teamId, onAdded }: any) 
 
     const [eventTypeID, setEventTypeID] = useState("");
     const [reoccurring, setReoccurring] = React.useState("");
+    const [startTime, setStartTime] = React.useState("10:30:00");
+    const [endTime, setEndTime] = React.useState("11:20:00");
+    const [startDate, setStartDate] = React.useState("");
+    const [endDate, setEndDate] = React.useState("");
     const [events, setEvents] = useState<EventItem[]>([]);
     
     /**
@@ -66,24 +71,26 @@ export const AddTeamEventModal = ({ open, onOpenChange, teamId, onAdded }: any) 
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="max-w-md max-h-100 overflow-y-auto">
+        <DialogContent className="max-w-md max-h-150 overflow-y-auto">
                 <DialogHeader>
                     <DialogTitle>Add a Team Event</DialogTitle>
                     <DialogDescription>Create a single or reocurring event for you and your team.</DialogDescription>
                 </DialogHeader>
                 <div>
-                    <Select value={eventTypeID} onValueChange={setEventTypeID}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Event Type" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            {teamEvents.map((e) => (
-                                <SelectItem key={e} value={e}>
-                                    {e}
-                                </SelectItem>
-                            ))}
-                        </SelectContent>
-                    </Select>
+                    <div className="grid w-full items-center gap-3 py-2">
+                        <Select value={eventTypeID} onValueChange={setEventTypeID}>
+                            <SelectTrigger>
+                                <SelectValue placeholder="Event Type" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {teamEvents.map((e) => (
+                                    <SelectItem key={e} value={e}>
+                                        {e}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
 
                     <div className="grid w-full items-center gap-3 py-2">
                         <Label>Day(s) of the Week</Label>
@@ -93,7 +100,7 @@ export const AddTeamEventModal = ({ open, onOpenChange, teamId, onAdded }: any) 
                                 <input
                                 type="checkbox"
                                 value={day}
-                                name="recurringDay"
+                                name="selectedDays"
                                 onChange={(e) => {
                                     if (e.target.checked) {
                                     setSelectedDays((s) => [...s, day]);
@@ -107,35 +114,108 @@ export const AddTeamEventModal = ({ open, onOpenChange, teamId, onAdded }: any) 
                             ))}
                         </div>
                     </div>
-                    
-                    <Select value={reoccurring} onValueChange={setReoccurring}>
-                        <SelectTrigger>
-                            <SelectValue placeholder="Reocurring Event" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value={false}>
-                                {"No"}
-                            </SelectItem>
-                            <SelectItem value={true}>
-                                {"Yes"}
-                            </SelectItem>
-                        </SelectContent>
-                    </Select>
 
-                    {reoccurring && (
-                        <Select value={selectedReoccurrType} onValueChange={setSelectedReoccurrType}>
-                            <SelectTrigger>
-                                <SelectValue placeholder="Type of Reocurrance" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                {reoccurrTypes.map((e) => (
-                                    <SelectItem key={e} value={e}>
-                                        {e}
+                    <div className="columns-1 sm:columns-2 gap-6">
+                        <div className="grid w-full items-center gap-3 py-2">
+                            <Label htmlFor="start_time">Start Time</Label>
+                            <Input
+                                type="time"
+                                id="start_time"
+                                value={startTime}
+                                onChange={(e) => setStartTime(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        <div className="grid w-full items-center gap-3 py-2">
+                            <Label htmlFor="end_time">End Time</Label>
+                            <Input
+                                type="time"
+                                id="end_time"
+                                value={endTime}
+                                onChange={(e) => setEndTime(e.target.value)}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    <div className="columns-1 sm:columns-2 gap-6">
+                        <div className="grid w-full items-center gap-3 py-2">
+                            <Label htmlFor="reoccurring">Reocurring Event</Label>
+                            <Select id ="reoccurring" value={reoccurring} onValueChange={setReoccurring}>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Yes/No" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value={false}>
+                                        {"No"}
                                     </SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                    )} 
+                                    <SelectItem value={true}>
+                                        {"Yes"}
+                                    </SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+
+                        <div className="grid w-full items-center gap-3 py-2">
+                        {reoccurring ? (
+                            <>
+                                <Label htmlFor="reoccurring">Type of Reocurrance</Label>
+                                <Select id = "selectedReoccurrType" value={selectedReoccurrType} onValueChange={setSelectedReoccurrType}>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Reocurr" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {reoccurrTypes.map((e) => (
+                                            <SelectItem key={e} value={e}>
+                                                {e}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </>                        
+                        )
+                        :   <div className="invisible">
+                                <Label>Placeholder</Label>
+                                <div className="h-10" />
+                            </div>
+                        } 
+                        </div>
+                    </div>
+                    <div className="columns-1 sm:columns-2 gap-6">
+                        <div className="grid w-full items-center gap-3 py-2">
+                            <Label htmlFor="start_date">
+                                {reoccurring ? "Start Date" : "Date"}
+                            </Label>
+                            <Input
+                                type="date"
+                                id="start_date"
+                                min="2025-01-01"
+                                max="2035-12-31"
+                                value={startDate}
+                                onChange={(e) => setStartDate(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        {reoccurring ? 
+                            <div className="grid w-full items-center gap-3 py-2">
+                                <Label htmlFor="end_date">End Date</Label>
+                                <Input
+                                    type="date"
+                                    id="end_date"
+                                    min="2025-01-01"
+                                    max="2035-12-31"
+                                    value={endDate}
+                                    onChange={(e) => setEndDate(e.target.value)}
+                                />
+                            </div>
+                        :   <div className="invisible">
+                                <Label>Placeholder</Label>
+                                <div className="h-10" />
+                            </div>
+                        } 
+                    </div>
 
                     {events 
                     ? <div className="lg:col-span-1 flex flex-col gap-4"><EventWidget events={events} /></div>
