fe042b0679e5031515c9303e78f21c29a8f84b4b
Will Ross <wross9959@gmail.com>
2026-01-16 17:28:35 -0400

    fixed config issue, and updated ICONS

--- apps/web/src/features/auth/pages/Register.tsx
+++ apps/web/src/features/auth/pages/Register.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { GalleryVerticalEnd } from "lucide-react";
 import { RegisterForm } from "@/features/auth/components/RegisterForm";
-import Logo from "@/assets/Logo.png";
+import Logo from "@/assets/Logo_Master.png";
+import Meshed_Icon from "@/assets/Icon.png";
 
 export const Register: React.FC = () => (
     <div className="grid h-screen lg:grid-cols-2">
         <div className="flex flex-col gap-4 p-6 md:p-10">
             <div className="flex justify-center gap-2 md:justify-start">
                 <a href="/" className="flex items-center gap-2 font-medium">
-                    <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                        <GalleryVerticalEnd className="size-4" />
+                    <div className="text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                        <img src={Meshed_Icon} alt="Meshed Icon" className="size-4" />
                     </div>
                     Meshed
                 </a>
