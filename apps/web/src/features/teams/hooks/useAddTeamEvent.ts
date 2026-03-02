aebea2a9c15c81d235bac064ddc21f51448d0f78
francescastewart <francesca.stewart@unb.ca>
2026-03-02 08:54:40 -0400

    158 - update useAddTeamEvent to remove logs

--- apps/web/src/features/teams/hooks/useAddTeamEvent.ts
+++ apps/web/src/features/teams/hooks/useAddTeamEvent.ts
@@ -63,21 +63,16 @@ export const useAddTeamEvent = () => {
                   const weekIndex = Math.floor(diffDays / 7);
                   if (weekIndex % intervalWeeks === 0) {
                       eventDates.push(new Date(d));
-                      console.log("Date being sent: " + d)
                   }
               } else if (selectedReoccurrType === "Monthly") {
                   if (d.getDate() === start.getDate()) {
                       eventDates.push(new Date(d));
-                      console.log("Date being sent: " + d)
                   }
               }
           }
-        
-        console.log("Date being recieved: " + start)
       } else {
           const start = new Date(startDate as Date);
           eventDates.push(start);
-          console.log("Date being sent: " + start)
       }
 
       for (const date of eventDates) {
