de80046ad2be7875d6e917cae57bc47c047c1564
francescastewart <francesca.stewart@unb.ca>
2026-03-02 09:02:22 -0400

    158 -  update addteameventmodal to not print startdate

--- apps/web/src/features/teams/modals/AddTeamEventModal.tsx
+++ apps/web/src/features/teams/modals/AddTeamEventModal.tsx
@@ -156,7 +156,6 @@ export const AddTeamEventModal = ({
         }
 
         try {
-            console.log(startDate)
             await addTeamEvent(
                 teamId,
                 teamFacilityId,
