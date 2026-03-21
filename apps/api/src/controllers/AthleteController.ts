ed5a2770cb47a60137c10db8129f237c8727b364
Will Ross <wross9959@gmail.com>
2026-03-21 16:23:23 -0300

    Error:   1:10  error  'Request' is defined but never used  @typescript-eslint/no-unused-vars

--- apps/api/src/controllers/AthleteController.ts
+++ apps/api/src/controllers/AthleteController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { AthleteModel } from "../models/AthleteModel";
 import { AuthedRequest } from "../middleware/authMiddleware";
 
