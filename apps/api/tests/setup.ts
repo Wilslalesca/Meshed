f7aa822f9cfac006f2b40b0fb270a1b35e1c41e7
Will Ross <wross9959@gmail.com>
2026-03-22 20:25:04 -0300

    try 2

--- apps/api/tests/setup.ts
+++ apps/api/tests/setup.ts
@@ -1,5 +1,4 @@
-process.env.NODE_ENV = 'test';
-process.env.JWT_ACCESS_SECRET = 'test-access-secret';
-process.env.JWT_REFRESH_SECRET = 'test-refresh-secret';
-process.env.COOKIE_DOMAIN = '';
-process.env.DATABASE_URL = 'postgres://test:test@localhost:5432/testdb';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' })
+process.env.NODE_ENV = 'test'
