55c31d7776556bcbb85388bd3dcbe59e0968f839
Will Ross <wross9959@gmail.com>
2026-03-22 20:17:00 -0300

    test

--- apps/api/tests/setup.ts
+++ apps/api/tests/setup.ts
@@ -2,4 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
+process.env.JWT_ACCESS_SECRET = "test-access-secret";
+process.env.JWT_REFRESH_SECRET = "test-refresh-secret";
+process.env.COOKIE_DOMAIN = "";
