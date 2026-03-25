5a03d6d1477ead65aa3027c208f175af59afd6f1
francescastewart <francesca.stewart@unb.ca>
2026-03-25 16:03:37 -0300

    Final fix event.test.ts

--- apps/api/tests/event.test.ts
+++ apps/api/tests/event.test.ts
@@ -41,7 +41,7 @@ describe('EventController.updateEventStatus', () => {
     await EventController.updateEventStatus(authReq, res)
 
     expect(EventModel.updateStatus).toHaveBeenCalledWith('event-1', 'approved', 'LGTM', mockUser.organizationId)
-    expect(EventEmailService.sendBookingStatusUpdateEmail).toHaveBeenCalledWith('event-1', mockUser.organizationId)
+    expect(EventEmailService.sendBookingStatusUpdateEmail).toHaveBeenCalledWith('event-1')
     expect(res.json).toHaveBeenCalledWith({ success: true })
   });
 
