499e18fe26d4fab6e8c42a1504307e219d2e525a
francescastewart <francesca.stewart@unb.ca>
2026-03-25 16:07:11 -0300

    Create facilities.test.ts

---
+++ apps/api/tests/facilities.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test, vi } from 'vitest';
+import { FacilityController } from '../src/controllers/FacilityController';
+import { FacilityModel } from '../src/models/FacilityModel';
+import { makeHttp } from './utils/http';
+import { mockFacility, mockUser } from './utils/fixtures';
+import { attachUser } from "./utils/auth"
+
+vi.mock('@/models/FacilityModel')
+
+//getAllFacilities
+describe('FacilityController.list', () => {
+  test('should return formatted facilities', async () => {
+    vi.clearAllMocks()
+    const { req, res } = makeHttp()
+    const authReq = attachUser(req, mockUser)
+
+    vi.mocked(FacilityModel.findAll).mockResolvedValue([mockFacility])
+    await FacilityController.list(authReq, res)
+
+    expect(FacilityModel.findAll).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith([mockFacility])
+  });
+});
