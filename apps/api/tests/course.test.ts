ecfc363edc7f0afe50bc50e374b741e5b9b9d856
francescastewart <francesca.stewart@unb.ca>
2026-03-26 10:57:04 -0300

    158 - fix status and inputs for coursetimes

--- apps/api/tests/course.test.ts
+++ apps/api/tests/course.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test, vi } from 'vitest';
 import { CourseController } from '../src/controllers/CourseController';
 import { CourseModel } from '../src/models/CourseModel';
 import { makeHttp } from './utils/http';
-import { mockCourseTime } from './utils/fixtures';
+import { mockCourseTime, mockCourseTimeInput } from './utils/fixtures';
 
 vi.mock('@/models/CourseModel')
 
@@ -10,13 +10,13 @@ describe('CourseController.createCourse', () => {
   test('should return formatted facilities', async () => {
     vi.clearAllMocks()
     const { req, res } = makeHttp()
-    req.body = mockCourseTime
+    req.body = mockCourseTimeInput
 
     vi.mocked(CourseModel.insertCourse).mockResolvedValue(mockCourseTime)
     await CourseController.createCourse(req, res)
 
     expect(CourseModel.insertCourse).toHaveBeenCalledTimes(1)
-    expect(res.json).toHaveBeenCalledWith()
+    expect(res.status).toHaveBeenCalledWith(200)
   });
 
   test('should NOT return formatted facilities', async () => {
