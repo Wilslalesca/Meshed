63ccf59827ed106118633d7b8c49087f0ec65180
Will Ross <wross9959@gmail.com>
2026-03-30 19:55:00 -0300

    fixed

--- apps/web/src/features/dashboard/views/managerView.tsx
+++ apps/web/src/features/dashboard/views/managerView.tsx
@@ -186,7 +186,7 @@ export const ManagerView = () => {
             
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
                 <div className="lg:col-span-2">
-                    <Card className="h-full">
+                    <Card className="max-h-[320px] overflow-hidden">
                         <CardHeader>
                             <CardTitle>Updates</CardTitle>
                         </CardHeader>
@@ -196,7 +196,7 @@ export const ManagerView = () => {
                     </Card>
                 </div>
                 <div className="lg:col-span-1">
-                    <EventWidget events={events} />
+                    {/* <EventWidget events={events} /> */}
                 </div>
             </div>
             <Card className="h-full">
